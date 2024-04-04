9c2de74f59dd590c652fbc8ca7e7b66ba4b606c4
Wellington-gc <wellington_gc@outlook.com>
2024-04-03 23:07:20 -0300

    Add user resource

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  name?: string;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,54 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      return await this.prisma.user.create({ data: createUserDto });
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async findAll() {
+    return await this.prisma.user.findMany();
+  }
+
+  async findOne(id: number) {
+    try {
+      return await this.prisma.user.findFirstOrThrow({
+        where: {
+          id: id,
+        },
+      });
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    try {
+      const name = updateUserDto.name;
+
+      return await this.prisma.user.update({
+        where: { id: id },
+        data: { name: name },
+      });
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async remove(id: number) {
+    try {
+      return await this.prisma.user.delete({ where: { id: id } });
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+}
